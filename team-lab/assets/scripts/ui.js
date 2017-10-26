ba12d5bf750703fb5b40abffec67b206a753e351
tacostacostacos <david.epstein@libertymutual.com>
2017-10-26 12:44:53 -0400

    Updated UI.js

--- team-lab/assets/scripts/ui.js
+++ team-lab/assets/scripts/ui.js
@@ -1,6 +1,10 @@
 'use strict'
 
+<<<<<<< HEAD
 const showBooksTemplate = require('../templates/lab.handlebars')
+=======
+const showBooksTemplate = require('../templates/book-listing.handlebars')
+>>>>>>> Added initial ui
 
 const getBooksSuccess = function (data) {
     const showPlaysHtml = showBooksTemplate({ books: data.books })
